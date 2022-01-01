2f6047d1a668dbce4797439b85f06972ff8bd51f
Cotelea Marius <marius.cotelea1003@gmail.com>
2022-01-01 19:00:43 +0200

    I created the restart and home buttons

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -6,7 +6,8 @@ public class MainMenuController : MonoBehaviour
 {
     public void PlayGame()
     {
-        //int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentInputModule.name);
+        int selectedCharacter = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+        GameManager.instance.CharIndex = selectedCharacter;
         SceneManager.LoadScene("Gameplay");
     }
 }
